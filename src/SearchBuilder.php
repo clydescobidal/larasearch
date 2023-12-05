dde22c933f3704601ce6fe1d5b972e67230fc193
clyde-escobidal <cleidoscope@gmail.com>
2023-12-05 11:18:47 +0800

    Update SearchBuilder.php

--- src/SearchBuilder.php
+++ src/SearchBuilder.php
@@ -2,64 +2,66 @@
 
 namespace Clydescobidal\Larasearch;
 
-use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Pagination\Paginator;
-use Illuminate\Support\Facades\Cache;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Query\Builder;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
 
 class SearchBuilder extends EloquentBuilder
 {
     public $searchQuery;
 
-    public $cachedData = [];
+    public $cachedData;
+
+    public $cache;
+
+    public Builder $builder;
 
     public function __construct(Model $model, string $searchQuery)
     {
         $this->query = DB::table($model->getTable());
         $this->model = $model;
         $this->searchQuery = $searchQuery;
+        $this->cache = config('larasearch.cache') ? Cache::tags($this->model::class) : null;
+        $this->builder = DB::table(config('larasearch.table'));
     }
 
-    /**
-     * Perform a search against the model's indexed data.
-     *
-     * @param  string  $query
-     * @return \Illuminate\Database\Eloquent\Builder
-     */
-    public function search() : \Illuminate\Database\Eloquent\Builder
+    public function search() : EloquentBuilder
     {
-        $searchableType = $this->model::class;
-        $searchableKeyName = $this->model->getKeyName();
-        $cache = config('larasearch.cache') ? Cache::tags($searchableType) : null;
+        $this->builder = $this->builder->select(['searchable_id', 'column', 'value'])
+            ->where('searchable_type', $this->model::class)
+            ->whereRaw("MATCH(value) AGAINST ('*{$this->searchQuery}*' IN BOOLEAN MODE)")
+            ->distinct();
 
-        if ($cache) {
-            $this->cachedData = $cache->get($this->searchQuery, Collection::make([]));
-
-            return $this;
-        }
-
-        $modelIds = DB::table(config('larasearch.table'))
-            ->select('searchable_id')
-            ->where('searchable_type', $searchableType)
-            ->where('value', 'LIKE', "%{$this->searchQuery}%")
-            ->distinct()
-            ->get();
-        $modelIds = $modelIds->pluck('searchable_id')->toArray();
-        $builder = $searchableType::whereIn($searchableKeyName, $modelIds);
-
-        if ($cache) {
-            $cache->put($this->searchQuery, $builder->get());
-        }
-
-        return $builder;
+        return $this;
     }
 
     public function get($columns = ['*'])
     {
-        return $this->cachedData;
+        $results = Collection::make();
+
+        if ($this->cache && $this->cache->has($this->searchQuery)) {
+            $results = $this->cache->get($this->searchQuery);
+        } else {
+            $results = $this->builder->get();
+            if ($this->cache) {
+                $this->cache->put($this->searchQuery, $results);
+            }
+        }
+
+        $results = $results instanceof Collection ? $results : Collection::make($results);
+        $searchableKeyName = $this->model->getKeyName();
+
+        return $results->map(function ($res) use ($searchableKeyName) {
+            return [
+                $searchableKeyName => $res->searchable_id,
+                $res->column => $res->value,
+            ];
+        });
     }
 
     public function paginate($perPage = null, $columns = ['*'], $pageName = 'page', $page = null)
